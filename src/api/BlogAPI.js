1b4e80da7bb356ae6743fb887da09ffa97f33079
runtime <erik.kroha@gmail.com>
2024-04-18 10:07:14 -0400

    added cors to api gateway for post & options. connected BlogAPI and usePosts to hit prod/hello returns 200 anhello api message

--- src/api/BlogAPI.js
+++ src/api/BlogAPI.js
@@ -10,12 +10,12 @@ export default axios.create({
     // params: {
     //      Key: {'itemId': 'id'}
     // },
-    headers: {
-        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-        "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify('success')
+    // headers: {
+    //     "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    //     "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    //     "Access-Control-Allow-Origin": "*",
+    // },
+     body: JSON.stringify('success')
 })
 
 //
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -12,34 +12,23 @@ const usePosts = (defaultSearchQuery) => {
 
     const search = async (query) => {
         try {
-            const response = await BlogAPI.get(`hello`, {
+            const response = await BlogAPI.post(`hello`, {
                 // params: {
                 //     'itemId': '1'
                 // },
                 // params: {
                 //     'Key': {'itemId': `${query}`},
                 // },
-                headers: {
-                    // "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-                    // "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-                    // "Access-Control-Allow-Origin": "http://localhost:3000",
-                    // "Content-Type": "application/json"
-
-
-
-                    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-                    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-                    "Access-Control-Allow-Origin": "*",
-
-
-                    // "Access-Control-Allow-Headers": "Content-Type,",
-                    // "Access-Control-Allow-Methods": "*",
-                    // "Access-Control-Allow-Credential":"false",
-                    // "Access-Control-Allow-Origin": "*",
-                }
+                // headers: {
+                //     "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                //     "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+                //     "Access-Control-Allow-Origin": "*",
+                //
+                //     //"Access-Control-Allow-Credential":"false",
+                // }
             });
-            console.log('[UsePosts] response.data', response);
-            setPosts(response);
+            console.log('[UsePosts] response', response.data);
+            //setPosts(response.data);
         } catch (err) {
             console.error(err)
         }
